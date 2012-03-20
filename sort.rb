24ba32f887c0d755ddc8ec8a218ea74181256146
skatkov <lunaticman@gmail.com>
2012-03-21 01:25:40 +0200

    merge algorithm solved - finally

--- sort.rb
+++ sort.rb
@@ -4,33 +4,38 @@ require 'test/unit'
 class TC_MyTest < Test::Unit::TestCase
 	def test_merge_sort
 		assert [5,4,1,8,7,2,6,3].merge_sort.eql?([1,2,3,4,5,6,7,8])
+		assert [6,7,4,5,3,1,2].merge_sort.eql?([1,2,3,4,	5,6,7])
 	end
 end 
 
 class Array
 	def merge_sort
 		return self if self.count.eql?(1)
-
-		#divide array two parts 
-		a = self[0..self.count/2-1]
-		b = self[self.count/2..self.count-1]
-
-		merge( a.merge_sort, b.merge_sort)
+		
+		merge(self[0..self.count/2-1].merge_sort, self[self.count/2..self.count-1].merge_sort)
 	end
 
 	def merge(a, b)
 		c = []
-		i = 0
-		j = 0
 
-		(a.count + b.count).times do |time| 
-			if a[i] < b[j]
-				c[time] = a[i];	i=i+1
-			elsif a[i] > b[j]
-				c[time] = b[j]; j=j+1 
-			end
-			puts c.inspect
+		while !a.empty? && !b.empty?
+			if a[0] < b[0]
+				c.push(a[0])
+				a.delete_at(0)
+			elsif a[0] > b[0]
+				c.push(b[0])
+				b.delete_at(0)
+			end			
 		end
+
+		while !a.empty?
+			c.push(a[0]); a.delete_at(0)
+		end
+
+		while !b.empty?
+			c.push(b[0]); b.delete_at(0)
+		end
+
 		return c
 	end
 end
