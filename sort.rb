2fdafced6bd8225007297aff73040ce81ae0d44d
Stanislav Katkov <stanislav.katkov@ixonos.com>
2012-03-21 18:19:22 +0200

    no endless loop now

--- sort.rb
+++ sort.rb
@@ -1,17 +1,21 @@
 
 require 'test/unit'
 
+
 class TC_MyTest < Test::Unit::TestCase
 	def test_merge_sort
 		assert [5,4,1,8,7,2,6,3].merge_sort.eql?([1,2,3,4,5,6,7,8])
 		assert [6,7,4,5,3,1,2].merge_sort.eql?([1,2,3,4,5,6,7])
 		
-		#endless loop - to fix
-		# assert [2,2,1,4,5,3,7,8,0, 9,0].merge_sort.eql?([0,0,1,2,2,3,4,5,7,8,9])
+	end
+
+	def test_merge_sort_with_duplicate_values
+		assert [2,2,1,4,5,3,7,8,0, 9,0].merge_sort.eql?([0,0,1,2,2,3,4,5,7,8,9])
 	end
 end
 
 class Array
+
 	def merge_sort
 		return self if self.count.eql?(1)
 		merge(self[0..self.count/2-1].merge_sort, self[self.count/2..self.count-1].merge_sort)
@@ -27,6 +31,9 @@ class Array
 			elsif a[0] > b[0]
 				c << b[0]
 				b.delete_at(0)
+			elsif a[0].eql?(b[0])
+				c << a[0]; c << b[0]
+				a.delete_at(0); b.delete_at(0)
 			end
 		end
 
