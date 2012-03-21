def7ba467605310ef8710826cf2f517ed5289779
Stanislav Katkov <stanislav.katkov@ixonos.com>
2012-03-21 14:13:29 +0200

    algorithm issue found

--- sort.rb
+++ sort.rb
@@ -4,14 +4,16 @@ require 'test/unit'
 class TC_MyTest < Test::Unit::TestCase
 	def test_merge_sort
 		assert [5,4,1,8,7,2,6,3].merge_sort.eql?([1,2,3,4,5,6,7,8])
-		assert [6,7,4,5,3,1,2].merge_sort.eql?([1,2,3,4,	5,6,7])
+		assert [6,7,4,5,3,1,2].merge_sort.eql?([1,2,3,4,5,6,7])
+		
+		#endless loop - to fix
+		# assert [2,2,1,4,5,3,7,8,0, 9,0].merge_sort.eql?([0,0,1,2,2,3,4,5,7,8,9])
 	end
-end 
+end
 
 class Array
 	def merge_sort
 		return self if self.count.eql?(1)
-		
 		merge(self[0..self.count/2-1].merge_sort, self[self.count/2..self.count-1].merge_sort)
 	end
 
@@ -20,12 +22,12 @@ class Array
 
 		while !a.empty? && !b.empty?
 			if a[0] < b[0]
-				c.push(a[0])
+				c << a[0]
 				a.delete_at(0)
 			elsif a[0] > b[0]
-				c.push(b[0])
+				c << b[0]
 				b.delete_at(0)
-			end			
+			end
 		end
 
 		while !a.empty?
